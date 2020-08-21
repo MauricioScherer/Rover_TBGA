067696ea1f40ca4e274a5c7235653664df36dfbc
b-r-a-s-i-l <b-r-a-s-i-l@users.noreply.github.com>
2020-08-21 12:17:35 -0300

    merge da master

--- Rover_TBGA/Assets/Scripts/Robo.cs
+++ Rover_TBGA/Assets/Scripts/Robo.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Robo : MonoBehaviour
 {
+    public PetriNet robo;
     // Start is called before the first frame update
     void Start()
     {
-        
+        robo = new PetriNet("Assets/RedesPetri/Robo.pflow");
     }
 
     // Update is called once per frame
     void Update()
     {
+        robo.ExecCycle();
+        
         
     }
 }
